3e8fdf7f3189fa4d8220f5a92996c5026f52ae1d
simon <simon.soderman@cgi.com>
2022-08-10 10:55:23 +0200

    meh

--- NoteTool/Configuration.cs
+++ NoteTool/Configuration.cs
@@ -9,10 +9,7 @@ namespace NoteTool {
         public string? TemplatesPath { get; set; }
         public string? IndexPath { get; set; }
 
-        public static string DefaultRootPath =>
-            System.IO.Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "note-tool");
-
-
+        public static string DefaultRootPath => System.IO.Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "note-tool");
         public static string ConfigPath => System.IO.Path.Join(DefaultRootPath, "config.json");
         public static string DefaultPath => System.IO.Path.Join(DefaultRootPath, "notes");
         public static string DefaultIndexPath => System.IO.Path.Join(DefaultRootPath, "index");
